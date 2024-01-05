56a2bc757d2578eec82aedf5b60ce58744f30c93
ShockTohp <shocktohp@shocktohp.xyz>
2024-01-05 11:21:54 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+/*
+ * main.js
+ * Copyright (C) 2024 kevin <kevin@kevin-ScrapboxOmega>
+ *
+ * Distributed under terms of the MIT license.
+ */
+	const myHeading = document.querySelector("h1");
+	myHeading.textContent = "Hello World";
